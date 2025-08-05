74d08d322e7180ecbf4a0d7cb6bd6762088370d9
Aditya Sandip Patil <aditya.patil23@vit.edu>
2025-08-06 00:44:11 +0530

    Fix API base URL and image loading for deployment

--- frontend/chatapp/src/lib/axios.js
+++ frontend/chatapp/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.MODE ==="developement" ? "http://localhost:5001/api" :"/api";
+//const BASE_URL = import.meta.env.MODE ==="developement" ? "http://localhost:5001/api" :"/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api";
 export const axiosInstance = axios.create({
     baseURL: "http://localhost:5001/api",
     withCredentials: true, // send  cookies with the request 
